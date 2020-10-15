d2ec2bb9b1b05fe694ea1ceefdd08f46434c52b4
Shenbaga Murugan <shenbagamurugan@gmail.com>
2020-10-15 06:12:53 +0530

    added file save and load features

--- phone_book.py
+++ phone_book.py
@@ -1,4 +1,25 @@
-phone_book = {}
+import os
+
+def greet():
+	print("Welcome to Phone Book!")
+
+def load_file(file_name, phn_bk):
+	if file_name == '':
+		file_name = 'contacts.pbk'
+	print("Loading from", file_name)
+	if not os.path.isfile(file_name):
+		with open(file_name, 'w') as pbk_file:
+			print(phn_bk, file = pbk_file)
+	with open(file_name, 'r') as pbk_file:
+		phn_bk = eval(pbk_file.read())
+	return phn_bk
+
+def save_file(file_name):
+	if file_name == '':
+		file_name = 'contacts.pbk'
+	with open(file_name, 'w') as pbk_file:
+		print(phone_book, file = pbk_file)
+		print("Writing to", file_name)
 
 def add_entry(name, phone_no):
 	if name in phone_book.keys():
@@ -38,11 +59,10 @@ def reverse_lookup(phone_no):
 	print("No entry for given number", phone_no)
 		
 def print_menu():
-	print("1. Add a new entry")
-	print("2. Lookup for an entry")
-	print("3. Delete an entry")
-	print("4. List all entries")
-	print("5. Reverse look up")
+	print("1. Add a new entry\t\t4. List all entries")
+	print("2. Lookup for an entry\t\t5. Reverse look up")
+	print("3. Delete an entry\t\t6. Load from file")
+	print("7. Save to file")
 	print("**Press any other key to exit**")
 	menu_opt = input("Enter your choice:")
 	handle_menu(menu_opt)
@@ -63,9 +83,18 @@ def handle_menu(menu_opt):
 	elif (menu_opt == "5"):
 		phone_no = input("Enter phone number to look up:")
 		reverse_lookup(phone_no)
+	elif (menu_opt == "6"):
+		file_name = input("Enter file to load:")
+		load_file(file_name)
+	elif (menu_opt == "7"):
+		file_name = input("Enter file to save:")
+		save_file(file_name)
 	else:
+		save_file('')
 		print("Exiting!")
 		return
 	print_menu()
-	
+
+phone_book = load_file('', {})
+greet()
 print_menu()

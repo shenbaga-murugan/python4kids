a81274c75e1ea94aadd7853b9b6d933739fb180a
Shenbaga Murugan <shenbagamurugan@gmail.com>
2020-10-18 22:31:25 +0530

    fixed a minor issue in lookup

--- phone_book.py
+++ phone_book.py
@@ -32,11 +32,11 @@ class PhoneBook:
 	def list_entries(self):
 		contact_list = "There are " + str(len(self.phone_book)) + " contacts"
 		for name in self.phone_book:
-			contact_list += "\n" + name + ": " + self.get_entry(name)
+			contact_list += "\n" + self.get_entry(name)
 		return contact_list
 				
 	def reverse_lookup(self, phone_no):
 		for name, phn_no in self.phone_book.items():
 			if phone_no == phn_no:
-				return name + ": " + self.get_entry(name)
+				return self.get_entry(name)
 		return "No entry for given number " + phone_no

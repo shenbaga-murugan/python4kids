be1ac4efb6c6b597d676f78913ba8ab79cd8f408
Shenbaga Murugan P <shenbagamurugan@gmail.com>
2020-12-04 21:00:28 +0530

    Added a sample encrypter

--- message_encrypter.py
+++ message_encrypter.py
@@ -8,13 +8,12 @@ def encrypt_message(message, key):
 def decrypt_message(message, key):
 	decrypted_message = ""
 	for letter in message:
-		new_key = (((int(key) % 128), ((int(key) % 128) - 32)) [int(key) > 32])
-		new_asci = (ord(letter) - new_key)
-		decrypted_message += chr((new_asci, new_asci + 96) [32 > new_asci])
+		new_asci = (ord(letter) - (int(key) % 128))
+		decrypted_message += chr((new_asci, new_asci + 128) [0 > new_asci])
 	return decrypted_message
 
 print("1. Encrypt Message\n2. Decrypt Message")
-menu_opt = input("Enter your choice: ")
+menu_opt = input("Enter your choice:")
 if(menu_opt == "1"):
 	message = input("Enter your Message: ")
 	key = input("Enter your key: ")
